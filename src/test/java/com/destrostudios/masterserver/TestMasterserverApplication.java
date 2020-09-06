6316f295d09a389091a52272321dea54455f9377
carlkuesters <carl.kuesters@youengineering.com>
2020-09-06 22:45:47 +0200

    Use unit tests in Jenkins.

---
+++ src/test/java/com/destrostudios/masterserver/TestMasterserverApplication.java
@@ -0,0 +1,13 @@
+package com.destrostudios.masterserver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestMasterserverApplication {
+
+	@Test
+	void univerShouldBeStable() {
+		assertEquals(42, 42);
+	}
+}
