5600a40b8dedbd1d50cd6c31849667a081756dfb
carlkuesters <carl.kuesters@gmail.com>
2023-10-20 23:12:19 +0200

    Fix profile when running on server.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'destrostudios',
       script: '/usr/lib/jvm/java-17-openjdk-amd64/bin/java',
-      args: '-jar destrostudios.jar',
+      args: '-jar destrostudios.jar --spring.profiles.active=prod',
       exp_backoff_restart_delay: 100,
     },
   ],
