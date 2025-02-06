ce69040c57271c74b66709bedb30047d8661172b
carlkuesters <carl.kuesters@gmail.com>
2025-02-06 19:35:27 +0100

    Trying to fix startup in container, WIP.

--- docker/app.sh
+++ docker/app.sh
@@ -1,3 +1,5 @@
+env
+
 openssl rsa -in $AUTH_PRIVATE_KEY -passin pass:$AUTH_PRIVATE_KEY_PASSPHRASE -outform DER -out private.der
 openssl rsa -in $AUTH_PRIVATE_KEY -passin pass:$AUTH_PRIVATE_KEY_PASSPHRASE -pubout -outform PEM -out public.pem
 
